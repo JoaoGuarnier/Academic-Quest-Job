2a6507e9cc25b11d81dca6589862aa4c3116a2ca
joaoguarnier <joaocarlosguarnier@hotmail.com>
2022-07-03 16:10:52 -0300

    criando job

--- src/main/java/com/academicquestjob/job/Tarefa.java
+++ src/main/java/com/academicquestjob/job/Tarefa.java
@@ -1,7 +1,7 @@
 package com.academicquestjob.job;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,20 +16,26 @@ public class Tarefa {
     private final long MINUTO = SEGUNDO * 60;
     private final long HORA = MINUTO * 60;
 
-    private String url = "https://ms-academicquest.herokuapp.com/teste";
-
+    private String urlJob = "https://ms-academicquest.herokuapp.com/tarefa/grupo/job";
 
     @Autowired
     private RestTemplate restTemplate;
 
 
-    @Scheduled(fixedDelay = SEGUNDO)
+    @Scheduled(cron = "0 0 23 * * *")
     public void taskExecutada() {
 
-        ResponseEntity<String> entity = restTemplate.getForEntity(url, String.class);
-        System.out.println(entity.getStatusCodeValue());
-
+        System.out.println("Executando job...");
+        ResponseEntity<String> entity = restTemplate.getForEntity(urlJob, String.class);
+        if (entity.getStatusCodeValue() == 200) {
+            System.out.println("Job executado com sucesso");
+        } else {
+            System.out.println("Job nao executado");
+        }
 
     }
+
+
+
 
 }
