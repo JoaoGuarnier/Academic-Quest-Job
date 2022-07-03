ab30ef7439ee1041be4b72ea5c9803900e0f9203
joaoguarnier <joaocarlosguarnier@hotmail.com>
2022-07-03 13:17:12 -0300

    criando job de teste

--- src/main/java/com/academicquestjob/job/Tarefa.java
+++ src/main/java/com/academicquestjob/job/Tarefa.java
@@ -1,24 +1,34 @@
 package com.academicquestjob.job;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+import org.springframework.web.client.RestTemplate;
 
 
 @Component
 @EnableScheduling
 public class Tarefa {
 
-    private final long SEGUNDO = 1000;
+    private final long SEGUNDO = 5000;
     private final long MINUTO = SEGUNDO * 60;
     private final long HORA = MINUTO * 60;
 
+    private String url = "https://ms-academicquest.herokuapp.com/teste";
 
-    @Scheduled(fixedDelay = MINUTO)
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @Scheduled(fixedDelay = SEGUNDO)
     public void taskExecutada() {
 
-        System.out.println("Executando taskExecutada...");
+        ResponseEntity<String> entity = restTemplate.getForEntity(url, String.class);
+        System.out.println(entity.getStatusCodeValue());
+
 
     }
 
